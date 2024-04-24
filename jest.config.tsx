4303e4943ae8e5b076c9ea12dab45b00d383a369
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-04-24 20:46:48 +0900

    jestの環境設定した

---
+++ jest.config.tsx
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./src/setupTests.ts'], // Jestでもセットアップファイルを利用する場合
+};
