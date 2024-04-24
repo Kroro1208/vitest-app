0f46440f10a6f1f883a7a51b9926b0c00774d0b9
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-04-24 22:50:56 +0900

    非同期処理のエラーテストかいた

---
+++ src/components/async_func.test.tsx
@@ -0,0 +1,20 @@
+import { delay } from "./async_func";
+
+it('delayが指定された時間後にメッセージを返す', async() => {
+    const result = await delay('Hello Vites', 3000);
+    expect(result).toBe('Hello Vites');
+})
+
+it('timeがマイナスの場合はエラーを返す', async() => {
+    try {
+        const result = await delay('Hello Vites', -1);
+    } catch (e: any) {
+        expect(e.message).toBe('timeは0以上で設定してください');        
+    }
+})
+
+// これでもOK
+it('timeがマイナスの場合はエラーを返す', async() => {
+    await expect(delay('Hello Vites', -2)).rejects.toThrow('timeは0以上で設定してください');
+})
+
